d5628945d66c4612ff5b47103d7920c0b0ce3358
ryan <jianronglu@upinnov.com>
2024-01-18 15:22:15 +0800

    textField 初体验

--- lib/widget/chore/widget_text_field.dart
+++ lib/widget/chore/widget_text_field.dart
@@ -10,6 +10,10 @@ class TextFieldDemo extends StatefulWidget {
 
 class _TextFieldDemoState extends BasePageState<TextFieldDemo> {
   late final TextEditingController _nameController;
+  String? _nameText;
+  String? _pwdText;
+  final FocusNode _nameFocusNode = FocusNode();
+  final FocusNode _pwdFocusNode = FocusNode();
 
   @override
   String getTitle() {
@@ -18,8 +22,27 @@ class _TextFieldDemoState extends BasePageState<TextFieldDemo> {
 
   @override
   void initState() {
-    _nameController = TextEditingController(text: '');
+    /*
+    _nameController = TextEditingController(text: '可以设置默认值');
+    _nameController.selection =
+        TextSelection(baseOffset: 2, extentOffset: _nameController.text.length);
+   */
+    _nameController = TextEditingController();
+    _nameController.addListener(() {
+      _nameText = _nameController.text;
+    });
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _nameFocusNode.addListener(() {
+      print('name_focus_node:${_nameFocusNode.hasFocus}');
+    });
+    _pwdFocusNode.addListener(() {
+      print('pwd_focus_node:${_pwdFocusNode.hasFocus}');
+    });
+    super.didChangeDependencies();
   }
 
   @override
@@ -29,14 +52,60 @@ class _TextFieldDemoState extends BasePageState<TextFieldDemo> {
       child: Column(
         children: [
           TextField(
-            controller: _nameController,
-            autofocus: true,//聚焦
-            keyboardType: TextInputType.number,//键盘类型
-            //cursorColor: Colors.red,//光标颜色
+            controller: _nameController, //使用controller 监听变化
+            autofocus: true, //聚焦
+            focusNode: _nameFocusNode,
+            keyboardType: TextInputType.text, //键盘类型
+            decoration: InputDecoration(
+              labelText: '用户名',
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: '请输入用户名',
+              hintStyle: TextStyle(color: Colors.grey),
+              prefixIcon: Icon(Icons.person),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+          TextField(
+            //controller:_pwdController,
+            onChanged: (value) {
+              //使用 onChanged 监听变化
+              _pwdText = value;
+            },
+            autofocus: false,
+            focusNode: _pwdFocusNode,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: '密码',
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: '请输入登录密码',
+              hintStyle: TextStyle(color: Colors.grey),
+              prefixIcon: Icon(Icons.lock),
+              focusedBorder: UnderlineInputBorder(
+                //获得焦点
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                //未获得焦点
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              // border: InputBorder.none,//隐藏下划线
+            ),
+          ),
+          ElevatedButton(
+            child: Text('登录'),
+            onPressed: () {
+              _pwdFocusNode.unfocus();
+              _pwdFocusNode.unfocus();
+              print('name:$_nameText, pwd:$_pwdText');
+            },
           ),
         ],
       ),
-
     );
   }
 }
